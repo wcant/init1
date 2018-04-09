a647e9ac49f2331aab6dd7734ba5d343c8baefca
Wes Cantrell <mail@init1.io>
2018-04-09 08:10:18 -0500

    retry

--- nginxStart.sh
+++ nginxStart.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d         -p 80:80         -v "$(pwd)/web:/usr/share/nginx/html"         clean4happy
+docker run -d         -p 80:80         -v "$(pwd)/web:/usr/share/nginx/html"         init1
